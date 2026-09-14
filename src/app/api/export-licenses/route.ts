51d036d36e6e9e08c60471486e743ac348c85f84
Sunynx <sunynx@github.com>
2026-09-14 13:57:17 +0700

    fix: Install xlsx-populate and ignore missing types

--- src/app/api/export-licenses/route.ts
+++ src/app/api/export-licenses/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server';
 import { supabase } from '@/lib/supabase';
+// @ts-expect-error: xlsx-populate does not have types
 import XlsxPopulate from 'xlsx-populate';
 import path from 'path';
 
